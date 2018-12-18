e822c578458fa0884fd2353581d14a46d1667ebd
tony <tonytenorioc@gmail.com>
2018-12-17 20:50:15 -0600

    Layout ready

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    let name='a.jpg';
+
+    $('.thumbs_style img').first().css('border','3px solid #fff');
+    $('#image').html("<img class='gallery_img_style' src='images/"+name+"' > ");
+
+});
